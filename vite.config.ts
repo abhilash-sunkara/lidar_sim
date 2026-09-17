d859bb6236765afe346ee0d60d923afee481cd38
Abhilash Sunkara <abhilaash.sunkara@gmail.com>
2026-09-16 21:07:04 -0500

    typo in config file

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/lidar-sim/', // Replace with your exact repository name
+  base: '/lidar_sim/', // Replace with your exact repository name
 })
