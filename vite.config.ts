f968547c9ec64712680871d5cbf1d09d97f096d0
Abhilash Sunkara <abhilaash.sunkara@gmail.com>
2026-09-16 21:10:27 -0500

    tailwind config

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  plugins: [tailwindcss()],
   base: '/lidar_sim/', // Replace with your exact repository name
 })
